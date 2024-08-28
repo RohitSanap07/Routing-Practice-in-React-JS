4b7382577f922851927401b212c988319e807924
RohitSanap07 <rohitsanap177@gmail.com>
2024-08-29 00:17:38 +0530

    navbar added

---
+++ src/navbar.js
@@ -0,0 +1,9 @@
+import "./navbar.css"
+function Navbar(){
+    return <div className="links" >
+       <a href="/">Profile</a>
+        <a href="/Skill">Skill</a>
+        <a href="/Education">Education</a>
+    </div>
+}
+export default Navbar;
